b0d09c1dd102484d1951bb40d2b9ad2d1794a74b
utkarsh669 <73828827+utkarsh669@users.noreply.github.com>
2021-01-25 18:59:22 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,38 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.4,
+            'friction':0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+        this.Visiblity=255
+      }
+      display(){
+         console.log(this.body.speed);
+        if(this.body.speed < 3){
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rectMode(CENTER);
+          strokeWeight(2);
+          stroke(0);
+          rect(0, 0, this.width, this.height);
+          pop();
+        
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           pop();
+         }
+         
+       }
+      
+       
+       
+}
